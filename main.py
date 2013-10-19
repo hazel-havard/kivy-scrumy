e5869626250a83ff6547e33039aee4b5df43f8ce
Isaac Havard <ihavard@gmail.com>
2013-10-19 13:42:02 -0700

    added edit functionality

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import kivy
 kivy.require('1.7.1')
 
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, ListProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class Story():
     def __init__(self, name, taskList):
@@ -25,17 +27,39 @@ class LoginScreen(Screen):
     
     def login(self):
         print("login: " + self.scrumIdInput.text + ", password: " + self.passwordInput.text)
-        main_screen = ScrumApp.get_running_app().root.get_screen('main')
-        main_screen.refresh()
+        mainScreen = ScrumApp.get_running_app().root.get_screen('main')
+        mainScreen.refresh()
         self.manager.current = 'main'
-        
+
+class TaskNameLabel(Label):
+    pass
+
+class TaskPersonLabel(Label):
+    pass
+
+class TaskNameInput(TextInput):
+    def save(self):
+        self.parent.task.name = self.text
+
+class TaskPersonInput(TextInput):
+    def save(self):
+        self.parent.task.person = self.text
+
 class StickyNote(BoxLayout):
-    name = StringProperty()
-    person = StringProperty()
-    def __init__(self, name, person, **kwargs):
+    background = ObjectProperty((1, 0, 0, 1))
+    task = ObjectProperty()
+    def __init__(self, task, **kwargs):
+        self.task = task
         super(StickyNote, self).__init__()
-        self.name = name
-        self.person = person
+
+    def on_touch_up(self, touch):
+        if self.collide_point(*touch.pos):
+            mainScreen = ScrumApp.get_running_app().root.get_screen('main')
+            mainScreen.selectedSticky = self
+            if mainScreen.mode == 'view':
+                self.background = (0, 0, 1, 1)
+                mainScreen.showButtons()
+            return True
 
 class TitleLabel(Label):
     pass
@@ -45,6 +69,10 @@ class GridData(GridLayout):
 
 class MainScreen(Screen):
     grid = ObjectProperty()
+    buttonArea = ObjectProperty()
+    stickies = ListProperty()
+    selectedSticky = ObjectProperty()
+    mode = StringProperty('view')
 
     oatTask = Task('make oatmeal', 'verify', 'wolf')
     eatTask = Task('eat grandma', 'in_progress', 'wolf')
@@ -53,7 +81,7 @@ class MainScreen(Screen):
 
     hairTask = Task('cut hair', 'verify', 'goldilocks')
     beTask = Task('break and enter', 'to_do', 'goldilocks')
-    vandalizeTask = Task('valdalize', 'to_do', 'golidlocks')
+    vandalizeTask = Task('vandalize', 'to_do', 'golidlocks')
     goldStory = Story('Goldilocks', [hairTask, beTask, vandalizeTask])
 
     stories = [wolfStory, goldStory]
@@ -78,7 +106,45 @@ class MainScreen(Screen):
                     cell = verify
                 elif task.status == 'done':
                     cell = done
-                cell.add_widget(StickyNote(task.name, task.person))
+                sticky = StickyNote(task)
+                self.stickies.append(sticky)
+                cell.add_widget(sticky)
+
+    def edit(self, instance):
+        self.mode = 'edit'
+        self.selectedSticky.clear_widgets()
+        self.selectedSticky.add_widget(TaskNameInput(text = self.selectedSticky.task.name))
+        self.selectedSticky.add_widget(TaskPersonInput(text = self.selectedSticky.task.person))
+
+    def exitEdit(self):
+        self.mode = 'view'
+        self.selectedSticky.clear_widgets()
+        self.selectedSticky.add_widget(TaskNameLabel(text = self.selectedSticky.task.name))
+        self.selectedSticky.add_widget(TaskPersonLabel(text = self.selectedSticky.task.person))
+
+    def delete(self, instance):
+        if self.selectedSticky.parent is not None:
+            self.selectedSticky.parent.remove_widget(self.selectedSticky)
+            self.stickies.remove(self.selectedSticky)
+
+    def move(self, instance):
+        pass
+
+    def showButtons(self):
+        editButton = Button(text='edit', on_press=self.edit)
+        deleteButton = Button(text='delete', on_press=self.delete)
+        moveButton = Button(text='move', on_press=self.move)
+        self.buttonArea.add_widget(editButton)
+        self.buttonArea.add_widget(deleteButton)
+        self.buttonArea.add_widget(moveButton)
+
+    def on_touch_up(self, touch):
+        if self.grid.collide_point(*touch.pos):
+            for sticky in self.stickies:
+                sticky.background = (1, 0, 0, 1)
+            self.buttonArea.clear_widgets()
+            if self.mode == 'edit' and not self.selectedSticky.collide_point(*touch.pos):
+                self.exitEdit()
 
 class ScrumScreenManager(ScreenManager):
     pass
